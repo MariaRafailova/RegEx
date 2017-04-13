c5607493c3eff58bf2f9ba3d55f63658e264580f
MariaRafailova <mrafailova@gmail.com>
2017-04-13 14:09:42 +0300

    Add files via upload

---
+++ HappinessIndex.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace HappinessIndex
+{
+    class HappinessIndex
+    {
+        static void Main()
+        {
+            Regex happy = new Regex(@"\:\)|\:D|\;\)|\:\*|\:\]|\;\]|\:\}|\;\}|\(\:|\*\:|c\:|\[\:|\[\;");
+
+            Regex sad = new Regex(@"\:\(|D\:\|\;\(|\:\[|\;\[|\:\{|\;\{|\)\:|\:c|\]\:|\]\;");
+
+            var input = Console.ReadLine();
+
+            MatchCollection happyMatches = happy.Matches(input);
+            MatchCollection sadMatches = sad.Matches(input);
+            
+            int happyCount = happyMatches.Count;
+            int sadCount = sadMatches.Count;
+
+            double happyIndex = happyCount / (double)sadCount;
+
+            string status = "";
+
+            if (happyIndex >=2)
+            {
+                status = ":D";
+            }
+            else if (happyIndex>1)
+            {
+                status = ":)";
+            }
+            else if (happyIndex == 1)
+            {
+                status = ":|";
+            }
+            else if (happyIndex <1)
+            {
+                status = ":(";
+            }
+
+            Console.WriteLine($"Happiness index: {happyIndex:F2} {status}");
+            Console.WriteLine($"[Happy count: {happyCount}, Sad count: {sadCount}]");
+        }
+    }
+}
---
+++ WordEncounter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+
+namespace WordEncounter
+{
+    public class WordEncounter
+    {
+        public static void Main()
+        {
+            List<string> result = new List<string>();
+
+            //var wordsInSent = new List<string>();
+
+            string filter = Console.ReadLine();
+
+            var character = filter[0];
+            int digit = int.Parse(filter[1].ToString());
+           
+            var sentence = Console.ReadLine();
+
+            while (sentence != "end")
+            {
+                Regex start = new Regex(@"^[A-Z]");
+                Regex end = new Regex(@"[.!?]$");
+
+                bool validStart = start.IsMatch(sentence);
+                bool validEnd = end.IsMatch(sentence);
+
+                if (validStart && validEnd)
+                {
+                    var newSentence = sentence
+                        .Split(new[] { ',', '.', ' ','!', '?',';', ':','(',')', '*'}, StringSplitOptions.RemoveEmptyEntries)
+                        .ToList();
+
+                    foreach (var word in newSentence)
+                    {
+                        int counter = 0;
+                        int index = word.IndexOf(character);
+                        while (index != -1)
+                        {
+                            counter++;
+                            index = word.IndexOf(character, index + 1);
+                        }
+
+                        if (counter == digit)
+                        {
+                            result.Add(word);
+                        }
+
+                    }
+                        //    string pattern = @"[a-zA-Z]*";
+
+                        //    Regex allWords = new Regex(pattern);
+
+                        //    MatchCollection words = Regex.Matches(sentence, pattern);
+
+                        //    foreach (Match word in words)
+                        //    {
+                        //        wordsInSent.Add(word.Value);
+                        //    }
+
+                        //    foreach (var w in wordsInSent)
+                        //    {
+                        //        int counter = 0;
+                        //        int index = w.IndexOf(character);
+                        //        while (index != -1)
+                        //        {
+                        //            counter++;
+                        //            index = w.IndexOf(character, index + 1);
+                        //        }
+
+                        //        if (counter == digit)
+                        //        {
+                        //            result.Add(w);
+                        //        }
+                        //     }
+                    }
+
+                sentence = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", result));
+
+        }
+    }
+}
