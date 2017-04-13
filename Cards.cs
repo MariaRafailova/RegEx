69b180b496d5ed8a4cf80686d2ebe915c416b445
MariaRafailova <mrafailova@gmail.com>
2017-04-13 14:08:13 +0300

    Add files via upload

---
+++ Cards.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Cards
+{
+    public class Cards
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"([1]?[0-9JQKA])([SHDC])";
+            
+            Regex regex = new Regex(pattern);
+
+            MatchCollection cards = Regex.Matches(input, pattern);
+
+            List<string> result = new List<string>();
+
+            foreach (Match card in cards)
+            {
+                int power = 0;
+
+                if (int.TryParse(card.Groups[1].Value, out power))
+                {
+                    if (power < 2 || power > 10)
+                    {
+                        continue;
+                    }
+                }                
+
+                result.Add(card.Value);
+            }
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
